5ae60985ceb06b5a9e9ae2d1a4b0f7910f5ffac8
unknown <phantrongvt1234@gmail.com>
2024-04-16 17:33:26 +0700

    chitietsp

---
+++ chitietsp.js
@@ -0,0 +1,40 @@
+function hien()
+{
+    x = document.querySelector("#hopchitiet1");
+    x.style.display="block";
+}
+function an()
+{
+    x = document.querySelector("#hopchitiet1");
+    x.style.display="none";
+}
+function hien1()
+{
+    x = document.querySelector("#hopchitiet2");
+    x.style.display="block";
+}
+function an1()
+{
+    x = document.querySelector("#hopchitiet2");
+    x.style.display="none";
+}
+function hien2()
+{
+    x = document.querySelector("#hopchitiet3");
+    x.style.display="block";
+}
+function an2()
+{
+    x = document.querySelector("#hopchitiet3");
+    x.style.display="none";
+}
+function hien3()
+{
+    x = document.querySelector("#hopchitiet4");
+    x.style.display="block";
+}
+function an3()
+{
+    x = document.querySelector("#hopchitiet4");
+    x.style.display="none";
+}
