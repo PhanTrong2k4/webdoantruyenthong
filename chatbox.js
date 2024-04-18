56c3c1c3058ad1a1ed08bcc24478b4c4042e8e1a
unknown <phantrongvt1234@gmail.com>
2024-04-19 00:50:37 +0700

    123

--- chatbox.js
+++ chatbox.js
@@ -2,27 +2,30 @@ function luachon1()
 {
    var x =  document.querySelector("#dongchatht");
    var bansaox = x.cloneNode(true);
-   bansaox.id="dongchatht1";
+   bansaox.id="dongchatht";
    x.innerHTML="Cửa hàng mở vào lúc 11h sáng tới 11h tối";
+   x.id="999"
    document.querySelector(".khungchatbox").appendChild(bansaox);
    var luuquery = document.querySelector(".chatuser");
 }
 function luachon2()
 {
-    var x =  document.querySelector("#dongchatht1");
+    var x =  document.querySelector("#dongchatht");
     var bansaox = x.cloneNode(true);
-    bansaox.id="dongchatht2";
+    bansaox.id="dongchatht";
     x.innerHTML="Địa chỉ cửa hàng là 125 Trương Công Định";
+    x.id="999"
     document.querySelector(".khungchatbox").appendChild(bansaox);
     var luuquery = document.querySelector(".chatuser");
 }
 function luachon3()
 {
-    var x =  document.querySelector("#dongchatht2");
+    var x =  document.querySelector("#dongchatht");
     var bansaox = x.cloneNode(true);
-    bansaox.id="dongchatht3";
+    bansaox.id="dongchatht";
     x.innerHTML="Vui lòng gọi cho đường dây hỗ trợ trực tuyến của cửa hàng là: <u>'883203231'</u>";
     document.querySelector(".khungchatbox").appendChild(bansaox);
+    x.id="999"
     var luuquery = document.querySelector(".chatuser");
 }
 function hien()
