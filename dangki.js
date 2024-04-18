f20f5e1124c00a71850a2bd6a37d3e371481cf2d
unknown <phantrongvt1234@gmail.com>
2024-04-18 18:43:08 +0700

    123

--- dangki.js
+++ dangki.js
@@ -2,134 +2,24 @@ function dangki()
 {
     var tendn = document.getElementById("tendn").value;
     var password1 = document.getElementById("password1").value;
-    var email = document.getElementById("email").value;
-    var sdt = document.getElementById("sdt").value;
-    var gender = document.querySelector('input[name="gender"]:checked').value;
-    if (password1 !== password2) {
-        alert("Mật khẩu và xác nhận mật khẩu không giống nhau!");
-        return;
-    }
-
-    var existingUserData = localStorage.getItem(tendn);
-    if (existingUserData) {
-        alert("Tên đăng nhập đã được sử dụng!");
-        return;
-    }
-
-
-    for (var key in localStorage) {
-        var userData = JSON.parse(localStorage.getItem(key));
-        if (userData && userData.email === email) {
-            alert("Email đã được sử dụng!");
-            return;
-        }
-    }
-
-    var user = {
-        "tendn": tendn,
-        "password": password1,
-        "email": email,
-        "sdt": sdt,
-        "gender": gender
-    };
-
-    localStorage.setItem(tendn, JSON.stringify(user));
-
-    document.getElementById("tendn").value = '';
-    document.getElementById("password1").value = '';
-    document.getElementById("password2").value = '';
-    document.getElementById("email").value = '';
-    document.getElementById("sdt").value = '';
-    document.getElementById("gender1").checked = false;
-    document.getElementById("gender2").checked = false;
-
-    alert("Đăng ký thành công!");
-}
-
-function dangnhap() 
-{
-    var tendn = document.getElementById("tendn").value;
-    var password = document.getElementById("password1").value;
-    var userJson = localStorage.getItem(tendn);
-    var user = JSON.parse(userJson);
-    if (user==null) 
-    {
-        alert("Sai tên đăng nhập hoặc mật khẩu");
-    }
-    else if (user.password === password && user.tendn == tendn) {
-        document.getElementById("tendn").value = '';
-        document.getElementById("password1").value = '';
-        alert("Đăng nhập thành công!");
-    }
-    else{
-        alert("Sai tên đăng nhập hoặc mật khẩu");
-    }
-    
-}
-
-function quenmatkhau() 
-{
-    var tendn = document.getElementById("tendn").value;
-    var email = document.getElementById("email").value;
-    var userJson = localStorage.getItem(tendn);
-    var user = JSON.parse(userJson);
-    if (user == null) 
-    {
-        alert("Email không tồn tại!");
-    }
-    else if(user.email==email && user.tendn == tendn) 
-    {
-        document.getElementById("tendn").value = '';
-        document.getElementById("email").value = '';
-        alert("Một tin nhắn đã được gửi tới email của bạn, vui lòng kiểm tra!");
-    }
-}
-
-function togglePassword1() {
-    var passwordField = document.getElementById("password1");
-    var toggleButton = document.getElementById("toggleButton");
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        
-    } else {
-        passwordField.type = "password";
-        
-    }
-}
-
-function togglePassword2() {
-    var passwordField = document.getElementById("password2");
-    
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        toggleButton.textContent = "Ẩn mật khẩu";
-    } else {
-        passwordField.type = "password";
-        toggleButton.textContent = "Hiện mật khẩu";
-    }
-}
-
-
-//-----------------------------------------------------
-function dangki() 
-{
-    var tendn = document.getElementById("tendn").value;
-    var password1 = document.getElementById("password1").value;
     var password2 = document.getElementById("password2").value;
     var email = document.getElementById("email").value;
     var sdt = document.getElementById("sdt").value;
-    var gender = document.querySelector('input[name="gender"]:checked').value;
 
-    if (password1 !== password2) {
-        alert("Mật khẩu và xác nhận mật khẩu không giống nhau!");
+    if (tendn=="" || password1=="" || password2=="" || email=="" || sdt=="") {
+        alert("Vui lòng nhập đầy đủ thông tin!");
         return;
     }
 
     var existingUserData = localStorage.getItem(tendn);
     if (existingUserData) {
         alert("Tên đăng nhập đã được sử dụng!");
+        return;
+    }
+
+
+    if (password1 !== password2) {
+        alert("Mật khẩu và xác nhận mật khẩu không giống nhau!");
         return;
     }
 
@@ -141,12 +31,22 @@ function dangki()
             return;
         }
     }
+
+
+    for (var key in localStorage) {
+        var userData = JSON.parse(localStorage.getItem(key));
+        if (userData && userData.sdt === sdt) {
+            alert("Số điện thoại đã được sử dụng!");
+            return;
+        }
+    }
+
+    
     var user = {
         "tendn": tendn,
         "password": password1,
         "email": email,
-        "sdt": sdt,
-        "gender": gender
+        "sdt": sdt
     };
 
     localStorage.setItem(tendn, JSON.stringify(user));
@@ -156,8 +56,6 @@ function dangki()
     document.getElementById("password2").value = '';
     document.getElementById("email").value = '';
     document.getElementById("sdt").value = '';
-    document.getElementById("gender1").checked = false;
-    document.getElementById("gender2").checked = false;
 
     alert("Đăng kí thành công!");
 }
@@ -168,8 +66,13 @@ function dangnhap()
 {
     var tendn = document.getElementById("tendn").value;
     var password = document.getElementById("password1").value;
+    
     var userJson = localStorage.getItem(tendn);
     var user = JSON.parse(userJson);
+    if (tendn=="" || password=="") {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+        return;
+    }
     if (user==null) 
     {
         alert("Sai tên đăng nhập hoặc mật khẩu");
@@ -178,6 +81,8 @@ function dangnhap()
         document.getElementById("tendn").value = '';
         document.getElementById("password1").value = '';
         alert("Đăng nhập thành công!");
+        
+        window.open("http://127.0.0.1:5500/baitapcuoikyweb.html");
     }
     else{
         alert("Sai tên đăng nhập hoặc mật khẩu");
@@ -191,6 +96,10 @@ function quenmatkhau()
     var email = document.getElementById("email").value;
     var userJson = localStorage.getItem(tendn);
     var user = JSON.parse(userJson);
+    if (tendn==""|| email=="") {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+        return;
+    }
     if (user == null) 
     {
         alert("Sai tên đăng nhập hoặc email!");
@@ -209,32 +118,20 @@ function quenmatkhau()
 
 function togglePassword1() {
     var passwordField = document.getElementById("password1");
-    var toggleButton = document.getElementById("toggleButton");
 
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        toggleButton.textContent = "Ẩn mật khẩu";
     } else {
         passwordField.type = "password";
-        toggleButton.textContent = "Hiện mật khẩu";
     }
 }
 
 function togglePassword2() {
     var passwordField = document.getElementById("password2");
-    var toggleButton = document.getElementById("toggleButton");
 
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        toggleButton.textContent = "Ẩn mật khẩu";
     } else {
         passwordField.type = "password";
-        toggleButton.textContent = "Hiện mật khẩu";
     }
-}
-
-
-//----------------------
-function dangnhap() {
-    window.location.href = "http://127.0.0.1:5500/baitapcuoikyweb.html";
 }
